66fbc3a8dfa3422c7aafeb2ac5eee6cde5c81d07
jomit626 <jomit626@gmail.com>
2021-12-25 16:58:36 +0800

    fix: misc: fix and move verify.sh.

--- verify.sh
+++ verify.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 set -e
-PROJ_FOLDER="$(dirname "$0")/../../.."
+PROJ_FOLDER="$(dirname "$0")"
 
 if ! [ -d $PROJ_FOLDER/paqFe/verify/db ] ; then
   $PROJ_FOLDER/src/test/resources/generate_verify_db.sh
 fi
 
 cd $PROJ_FOLDER
-#sbt sbt 'runMain GetCompressorVerilog;test;'
+sbt 'runMain GetCompressorVerilog;test;'
 cd -
 
 $PROJ_FOLDER/sim/sim.sh
