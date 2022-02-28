a3e0f722bae57bcd66b8538cc42ab5f8886f0b07
jomit626 <jomit626@gmail.com>
2022-02-28 20:30:35 +0800

    impl: temp for mixer impl

---
+++ src/main/scala/Mixer.scala
@@ -0,0 +1,484 @@
+import chisel3._
+import chisel3.util._
+
+import chisel3.experimental.dataview._
+import chisel3.experimental.conversions._
+
+import types._
+import ram._
+import models.RamInitUnit
+import internal.firrtl.Width
+import os.truncate
+
+class MixerParameter(n : Int = 4) {
+  val nFeatures = n
+
+  val WeightWidth = 17.W
+  val XWidth = 12.W
+  val lossWidth = 18.W
+
+  val WeightInitVal = (1 << (WeightWidth.get + 1)) / n
+}
+
+class BitProbsCtxBundle(n : Int) extends Bundle {
+  val bit = UInt(1.W)
+  val ctx = UInt(8.W)
+  val probs = Vec(n, UInt(12.W))
+  val last = Bool()
+}
+
+object XsBundle {
+  def apply(n : Int) = {
+    Vec(n, SInt(12.W))
+  }
+}
+
+object WeightsBundle {
+  def apply(n : Int) = {
+    Vec(n, SInt(17.W))
+  }
+}
+
+import  models.RamInitUnit
+
+class DPRam[T <: Data](t : T, addr : Width) extends Module {
+  val io = IO(new Bundle {
+    val wea = Input(Bool())
+    val addra = Input(UInt(addr))
+    val dina = Input(t)
+
+    val enb = Input(Bool())
+    val addrb = Input(UInt(addr))
+    val dob = Output(t)
+  })
+
+  val mem = SyncReadMem(1 << addr.get, t)
+
+  io.dob := mem.read(io.addrb, io.enb)
+
+  when(io.wea) {
+    mem.write(io.addra, io.dina)
+  }
+}
+
+class PredictUpdateEngineCtrBundle extends Bundle {
+  val bit = UInt(1.W)
+  // val wHarzard = Bool()
+} 
+
+class MACC(AWidth : Width, BWidth : Width, CWidth : Width) extends Module {
+  val latency = 3
+
+  val io = IO(new Bundle {
+    val ce = Input(Bool())
+
+    val a = Input(SInt(AWidth))
+    val b = Input(SInt(BWidth))
+    val reload = Input(Bool())
+
+    val acc = Output(SInt(CWidth))
+  })
+
+  val a = RegEnable(io.a, io.ce)
+  val b = RegEnable(io.b, io.ce)
+  val reload = ShiftRegister(io.reload, 2 , true.B, io.ce)
+
+  val mul = RegEnable(a * b, io.ce)
+  val lastResult = Wire(SInt(CWidth))
+  val acc = RegEnable(mul + lastResult, io.ce)
+
+  lastResult := Mux(reload, 0.S, acc)
+
+  io.acc := acc
+}
+
+// c - a * b
+class MS (AWidth : Width, BWidth : Width, CWidth : Width) extends Module {
+  val latency = 3
+  val io = IO(new Bundle {
+    val ce = Input(Bool())
+
+    val a = Input(SInt(AWidth))
+    val b = Input(SInt(BWidth))
+    val c = Input(SInt(CWidth))
+
+    val out = Output(SInt(CWidth))
+  })
+
+  val a = RegEnable(io.a, io.ce)
+  val b = RegEnable(io.b, io.ce)
+  val c = ShiftRegister(io.c, 2, io.ce)
+
+  val mul = RegEnable(a * b, io.ce)
+  val out = RegEnable(c + (mul >> 16), io.ce)
+
+  io.out := out
+}
+
+class PredictPE()(implicit p : MixerParameter) extends Module {
+  val io = IO(new Bundle {
+    val W = Flipped(DecoupledIO(WeightsBundle(p.nFeatures)))
+    val X = Flipped(DecoupledIO(XsBundle(p.nFeatures)))
+    val ctrl = Flipped(DecoupledIO(new PredictUpdateEngineCtrBundle()))
+
+    val P = ValidIO(new BitProbBundle())
+
+    val updateStrm = ValidIO(new Bundle {
+      val w = SInt(p.WeightWidth)
+      val x = SInt(p.XWidth)
+    })
+  })
+
+  // ctrl signals
+  val xLoad = Wire(Bool())
+  val wLoad = Wire(Bool())
+  val bitLoad = Wire(Bool())
+  val wxSel = Wire(UInt(log2Ceil(p.nFeatures + 1).W))
+
+  val maccReload = Wire(Bool())
+  val maccCE = Wire(Bool())
+
+  val pOut = Wire(Bool())
+
+  // data path
+  // X * W
+  val X = RegEnable(io.X.bits, xLoad)
+  val W = RegEnable(io.W.bits, wLoad)
+  val bit0 = RegEnable(io.ctrl.bits.bit, bitLoad)
+
+  val x = X(wxSel)
+  val w = W(wxSel)
+
+  val macc = Module(new MACC(p.XWidth, p.WeightWidth, 32.W))
+  macc.io.a := x
+  macc.io.b := w
+  macc.io.ce := maccCE
+  macc.io.reload := maccReload
+
+  val bit = ShiftRegister(bit0, 3, maccCE)
+  val prob = Squash(macc.io.acc >> 16)
+
+  // Ctrl singls gen
+  val cntCE = Wire(Bool())
+  val (cnt, cntWrp) = Counter(0 until p.nFeatures, cntCE)
+
+  val sIdle :: sWorking :: Nil = Enum(2)
+  val stateNxt = Wire(UInt())
+  val state = RegNext(stateNxt)
+
+  val takeInput = io.X.fire && io.W.fire && io.ctrl.fire
+  stateNxt := state
+  switch(state) {
+    is(sIdle) {
+      when(takeInput) {
+        stateNxt := sWorking
+      }
+    }
+
+    is(sWorking) {
+      when(cntWrp && ~takeInput) {
+        stateNxt := sIdle
+      }
+    }
+  }
+
+  val inIdle = state === sIdle
+  val inWorking = state === sWorking
+  cntCE := inWorking
+
+  xLoad := (inIdle && takeInput) || (inWorking && cntWrp)
+  wLoad := xLoad
+  bitLoad := wLoad
+  wxSel := cnt
+
+  maccReload := cnt === 0.U
+  maccCE := true.B
+
+  pOut := ShiftRegister(cntWrp, 3)
+
+  // outputs
+  io.X.ready := inIdle || (inWorking && cntWrp)
+  io.W.ready := io.X.ready
+  io.ctrl.ready := io.X.ready
+
+  io.P.bits.bit := bit
+  io.P.bits.prob := prob
+  io.P.bits.last := false.B //DontCare
+  io.P.valid := pOut
+
+  io.updateStrm.bits.x := ShiftRegister(x, p.nFeatures + macc.latency)
+  io.updateStrm.bits.w := ShiftRegister(w, p.nFeatures + macc.latency)
+  io.updateStrm.valid := ShiftRegister(inWorking, p.nFeatures + macc.latency, false.B, true.B)
+}
+
+
+class LossCalPE()(implicit p : MixerParameter) extends Module {
+  val latency = 1
+
+  val io = IO(new Bundle {
+    val P = Input(ValidIO(new BitProbBundle()))
+
+    val loss = Output(SInt(p.lossWidth))
+
+    val updateStrmCAS = Input(ValidIO(new Bundle {
+      val w = SInt(p.WeightWidth)
+      val x = SInt(p.XWidth)
+    }))
+
+    val updateStrm = Output(ValidIO(new Bundle {
+      val w = SInt(p.WeightWidth)
+      val x = SInt(p.XWidth)
+    }))
+  })
+  // ctrl signals
+  val lossLoad = Wire(Bool())
+
+  // data path
+  val prob = io.P.bits.prob
+  val bit = io.P.bits.bit
+  val probExpect = 0.U(p.lossWidth) | Cat(bit, 0.U(12.W))
+
+  val lr = 10.U(6.W)
+  val lossCal = (probExpect - prob).asSInt * lr  // zero ext first?
+  val loss = RegEnable(lossCal, lossLoad)
+
+  // ctrl signals gne
+  lossLoad := io.P.valid
+
+  // output
+  io.loss := loss
+
+  io.updateStrm := Pipe(io.updateStrmCAS)
+}
+
+class UpdatePE()(implicit p : MixerParameter) extends Module {
+  val io = IO(new Bundle {
+    val loss = Input(SInt(p.lossWidth))
+
+    val updateStrm = Flipped(ValidIO(new Bundle {
+      val w = SInt(p.WeightWidth)
+      val x = SInt(p.XWidth)
+    }))
+
+    val wStrm = ValidIO(SInt(p.WeightWidth))
+  })
+  // ctrl signals
+
+  // data path
+  val ms = Module(new MS(p.XWidth, p.lossWidth, p.WeightWidth))
+  ms.io.ce := true.B
+  ms.io.a := io.updateStrm.bits.x
+  ms.io.b := io.loss
+  ms.io.c := io.updateStrm.bits.w
+
+  // ctrl signals gen
+
+  // io
+  io.wStrm.bits := ms.io.out
+  io.wStrm.valid := ShiftRegister(io.updateStrm.valid, ms.latency, false.B, true.B)
+}
+
+class PredictUpdateEngine(n : Int)(implicit p : MixerParameter) extends Module {
+  val io = IO(new Bundle {
+    val W = Input(DecoupledIO(WeightsBundle(p.nFeatures)))
+    val X = Input(DecoupledIO(XsBundle(p.nFeatures)))
+    val ctrl = Input(DecoupledIO(new PredictUpdateEngineCtrBundle()))
+
+    val P = Output(ValidIO(new BitProbBundle()))
+    val Wu = Output(ValidIO(SInt(p.WeightWidth)))
+  })
+
+  // ctrl signals
+  val xLoad = Wire(Bool())
+  val wLoad = Wire(Bool())
+  val bitLoad = Wire(Bool())
+  val wxSel = Wire(UInt(log2Ceil(n + 1).W))
+
+  val maccReload = Wire(Bool())
+  val maccCE = Wire(Bool())
+
+  val pOut = Wire(Bool())
+  val lossLoad = Wire(Bool())
+
+  val msCE = Wire(Bool())
+  val wuOut = Wire(Bool())
+
+  // data path
+  // X * W
+  val X = RegEnable(io.X.bits, xLoad)
+  val W = RegEnable(io.W.bits, wLoad)
+  val bit0 = RegEnable(io.ctrl.bits.bit, bitLoad)
+
+  val x = X(wxSel)
+  val w = W(wxSel)
+
+  val macc = Module(new MACC(p.XWidth, p.WeightWidth, 32.W))
+  macc.io.a := x
+  macc.io.b := w
+  macc.io.ce := maccCE
+  macc.io.reload := maccReload
+
+  // cal loss
+  val bit = ShiftRegister(bit0, 3, maccCE)
+  val prob = Squash(macc.io.acc >> 16)
+  val probExpect = Cat(bit, 0.U(11.W))
+
+  val lr = 55.U(6.W)
+  val lossCal = (probExpect - prob).asSInt * lr  // zero ext first?
+  val loss = RegEnable(lossCal, lossLoad)
+
+  // W + X * loss
+  val x2 = ShiftRegister(x, p.nFeatures + macc.latency, maccCE)
+  val w2 = ShiftRegister(w, p.nFeatures + macc.latency, maccCE)
+
+  val ms = Module(new MS(p.XWidth, 12.W, p.WeightWidth))
+  ms.io.ce := msCE
+  ms.io.a := x2
+  ms.io.b := loss
+  ms.io.c := w2
+
+  // Ctrl singls generation
+  //val s0 :: s1 :: s2 :: s3
+  val cntCE = Wire(Bool())
+  val (cnt, cntWrp) = Counter(0 until p.nFeatures, cntCE)
+
+  val sIdle :: sWorking :: Nil = Enum(2)
+  val stateNxt = Wire(UInt())
+  val state = RegNext(stateNxt)
+
+  val takeInput = io.X.fire && io.W.fire && io.ctrl.fire
+  switch(state) {
+    is(sIdle) {
+      when(takeInput) {
+        stateNxt := sWorking
+      }
+    }
+
+    is(sWorking) {
+      when(cntWrp && ~takeInput) {
+        stateNxt := sIdle
+      }
+    }
+  }
+
+  val inIdle = state === sIdle
+  val inWorking = state === sWorking
+  cntCE := inWorking
+
+  xLoad := inIdle || (inWorking && cntWrp)
+  wLoad := inIdle || (inWorking && cntWrp)
+  bitLoad := inIdle || (inWorking && cntWrp)
+  wxSel := cnt
+
+  maccReload := cnt === 0.U
+  maccCE := inWorking
+
+  pOut := ShiftRegister(cntWrp, 3)
+  lossLoad := pOut
+
+  // outputs
+  io.P.bits.bit := bit
+  io.P.bits.prob := prob
+  io.P.bits.last := DontCare
+  io.P.valid := pOut
+
+  io.Wu.bits := ms.io.out
+  io.Wu.valid := wuOut
+}
+
+class MixerPE(n : Int, ForceFirstOutput : Boolean) extends Module {
+  val WeightInitVal = (1 << 16) / n
+  
+  val WeightInitVec = VecInit(Seq.fill(n){WeightInitVal.S(17.W)})
+  val io = IO(new Bundle {
+    val in = Flipped(DecoupledIO(new BitProbsCtxBundle(n)))
+    val out = ValidIO(new BitProbBundle)
+
+    val status = new StatusBundle
+  })
+
+  val fire = ShiftRegisters(io.in.fire, 3)
+  val X = ShiftRegisters(VecInit(io.in.bits.probs.map(Stretch(_))), 2)
+  val ctx = ShiftRegisters(io.in.bits.ctx, 3)
+  val bit = ShiftRegisters(io.in.bits.bit, 2)
+  val last = RegNext(io.in.bits.last)
+
+  val mem = Module(new DPRam(chiselTypeOf(WeightInitVec), log2Up(80).W)).io
+  mem.enb := io.in.fire
+  mem.addrb := io.in.bits.ctx
+
+  val Wr = mem.dob
+
+  val ramInit = Module(new RamInitUnit(log2Up(80))).io
+  ramInit.in.bits := last(0)
+  ramInit.in.valid := fire(0)
+
+  val W0sel = Wire(Bool())
+  val W0 = Wire(chiselTypeOf(Wr))
+  val W1 = RegNext(W0)
+
+  val mul = (X(0) zip W0).map {case (a, b) => a * b}
+  val p = Squash(TreeReduce[SInt](mul, _ + _) >> 16)
+  val p_d = RegNext(p)
+  val p_exp = Cat(bit(1), 0.U(11.W))
+
+  val lr = 55.U(6.W)
+  val loss = (p_exp - p_d).asSInt * lr
+
+  val Wu = RegNext(VecInit((0 until n).map(i => (W1(i) - X(1)(i) * loss).asTypeOf(SInt(17.W)))))
+
+  W0 := Mux(W0sel, Wu, Wr)
+  mem.wea := fire(2) || ramInit.wen
+  mem.addra := Mux1H(Seq(
+                      (fire(2), ctx(2)),
+                      (ramInit.wen, ramInit.waddr),
+                      ))
+  mem.dina := Mux1H(Seq(
+                      (fire(2), Wu),
+                      (ramInit.wen, WeightInitVec),
+                      ))
+
+  val harzard = io.in.bits.ctx === ctx(0) && fire(0)
+  val harzard_d = RegNext(harzard, false.B)
+  val harzard_dd = RegNext(harzard_d, false.B)
+
+  W0sel := harzard_dd
+
+  io.in.ready := ~harzard
+
+  io.out.valid := fire(0)
+  io.out.bits.bit := bit(0)
+  io.out.bits.last := last
+  if(ForceFirstOutput) {
+    val first = RegInit(true.B)
+    when(~io.status.initDone) {
+      first := true.B
+    }.elsewhen(io.out.valid) {
+      first := false.B
+    }
+
+    io.out.bits.prob := Mux(first, 2048.U, p)
+  } else {
+    io.out.bits.prob := p
+  }
+
+  io.status := ramInit.status
+}
+
+class Mixer(n : Int) extends Module {
+  val nProb = n
+  
+  val io = IO(new Bundle {
+    val in = Vec(8, Flipped(DecoupledIO(new BitProbsCtxBundle(n))))
+    val out = Vec(8, ValidIO(new BitProbBundle))
+
+    val status = new StatusBundle
+  })
+
+  val PEs = (0 until 8).map(i => Module(new MixerPE(n, i == 0)).io)
+  for(i <- 0 until 8) {
+    PEs(i).in <> io.in(i)
+    PEs(i).out <> io.out(i)
+  }
+  io.status.initDone := TreeReduce[Bool](PEs.map(_.status.initDone), _ & _)
+}
---
+++ src/test/scala/Mixer.scala
@@ -0,0 +1,336 @@
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.BundleLiterals._
+
+import chiseltest._
+
+import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import verifydata._
+import types._
+
+import java.io._
+import com.github.tototoshi.csv._
+import chisel3.experimental.VecLiterals._
+
+import LocalHelpers._
+import testhelpers.Helpers._
+
+import Helpers._
+
+class MixerSubModuleSpec
+  extends AnyFlatSpec
+  with ChiselScalatestTester
+  with Matchers {
+  
+  assert(TreeReduce[Int](Seq(1,2,3,4), _ + _) == 10)
+  assert(TreeReduce[Int](Seq(1,2,3,4,5), _ + _) == 15)
+
+  behavior of "Mixer Prediction Function"
+  val mixerPredictDB = new VerifyData("./paqFe/verify/db/mixer-predict")
+  for(line <- mixerPredictDB.data) {
+    val data_name = line(0)
+    val input_file = line(1)
+    val output_file = line(2)
+    
+    val testVectorLen = CSVReader.open(new File(output_file)).readNext().get.size
+    // X(n), W(n), bit, prob
+    assert((testVectorLen - 2) % 2 == 0)
+    val nFeatures = (testVectorLen - 2) / 2
+
+    implicit val p = new MixerParameter(nFeatures)
+    it should s"match software model with data: $data_name" in {
+      test(new PredictPE())
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { c =>
+        c.init()
+
+        c.test(output_file)
+      }
+    }
+
+  }
+
+  behavior of "Loss Calcuation"
+  val mixerLossDB = new VerifyData("./paqFe/verify/db/mixer-loss")
+  for(line <- mixerLossDB.data) {
+    val data_name = line(0)
+    val input_file = line(1)
+    val output_file = line(2)
+    
+    implicit val p = new MixerParameter()
+    it should s"match software model with data: $data_name" in {
+      test(new LossCalPE())
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { c =>
+        c.init()
+        c.test(output_file)
+      }
+    }
+  }
+
+  behavior of "Weight Update"
+  val mixerUpdateDB = new VerifyData("./paqFe/verify/db/mixer-update")
+  for(line <- mixerUpdateDB.data) {
+    val data_name = line(0)
+    val input_file = line(1)
+    val output_file = line(2)
+
+    val testVectorLen = CSVReader.open(new File(output_file)).readNext().get.size
+    // X(n), W(n),  Wu(n), loss
+    assert((testVectorLen - 1) % 3 == 0)
+    val nFeatures = (testVectorLen - 1) / 3
+
+    implicit val p = new MixerParameter(nFeatures)
+    it should s"match software model with data: $data_name" in {
+      test(new UpdatePE())
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { c =>
+        c.init()
+        c.test(output_file)
+      }
+    }
+  }
+  /*
+  behavior of "Mixer"
+
+  val db = new VerifyData("./paqFe/verify/db/mixer")
+  for(line <- db.data) {
+    val data_name = line(0)
+    val input_file = line(1)
+    val output_file = line(2)
+    it should s"match software model with data: $data_name" in {
+      test(new Mixer(5))
+      .withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { c =>
+      
+        MixerDUT.init(c)
+        statusWaitInitDone(c.clock, c.io.status)
+
+        MixerDUT.test(c, output_file)
+      }
+    }
+  }
+  */
+}
+object Helpers {
+
+implicit class LossCalPEDUT(c : LossCalPE)(implicit p : MixerParameter) {
+  def init() = {
+    c.io.P.initSource()
+    c.io.P.setSourceClock(c.clock)
+  }
+
+  def test( dbFile : String) = {
+    val reader = CSVReader.open(new File(dbFile))
+    val data = reader.all().map{ _.map(_.toInt)}.toIndexedSeq
+
+    val bd = new BitProbBundle()
+    for(line <- data) {
+      val bit = line(0)
+      val prob = line(1)
+      val loss = line(2)
+
+      c.io.P.enqueueNow(bd.Lit(_.bit -> bit.U, _.prob -> prob.U, _.last -> false.B))
+      c.clock.step()
+      c.io.loss.expect(loss.S(p.lossWidth))
+    }
+  }
+}
+
+implicit class PredictPEDUT(c : PredictPE)(implicit p : MixerParameter) {
+  def init() = {
+    c.io.W.initSource()
+    c.io.W.setSourceClock(c.clock)
+
+    c.io.X.initSource()
+    c.io.X.setSourceClock(c.clock)
+
+    c.io.ctrl.initSource()
+    c.io.ctrl.setSourceClock(c.clock)
+
+    c.io.P.initSink()
+    c.io.P.setSinkClock(c.clock)
+
+    c.io.updateStrm.initSink()
+    c.io.updateStrm.setSinkClock(c.clock)
+  }
+
+  def test(dbFile : String) = {
+    val reader = CSVReader.open(new File(dbFile))
+    val data = reader.all().map{ _.map(_.toInt)}.toIndexedSeq
+    val nFeatures = p.nFeatures
+
+    fork {
+      // feed X
+      // X(n), W(n), bit, prob
+      val sfrom = 0
+      val suntil = sfrom + nFeatures
+      for(line <- data) {
+        val data = line.slice(sfrom, suntil)
+        assert(data.length == nFeatures)
+        c.io.X.enqueue(Vec.Lit(data.map(_.S(p.XWidth)):_*))
+      }
+    }.fork {
+      // feed W
+      // X(n), W(n), bit, prob
+      val sfrom = nFeatures
+      val suntil = sfrom + nFeatures
+      for(line <- data) {
+        val data = line.slice(sfrom, suntil)
+        c.io.W.enqueue(Vec.Lit(data.map(_.S(p.WeightWidth)):_*))
+      }
+    }.fork {
+      // feed bit
+      val idx = nFeatures * 2
+      val db = new PredictUpdateEngineCtrBundle()
+      for(line <- data) {
+        val data = line(idx)
+        c.io.ctrl.enqueue(db.Lit(_.bit -> data.U))
+      }
+    }.fork {
+      // expect P
+      val bIdx = nFeatures * 2
+      val pIdx = nFeatures * 2 + 1
+      val db = new BitProbBundle()
+      for(line <- data) {
+        val bit = line(bIdx)
+        val prob = line(pIdx)
+        c.io.P.expectDequeue(db.Lit(_.bit -> bit.U, _.prob -> prob.U, _.last -> false.B))
+      }
+    }.fork {
+      // expect stream, focus on timming
+      for(line <- data) {
+        val X = line.slice(0, nFeatures)
+        val W = line.slice(nFeatures, nFeatures * 2)
+
+        var valid = c.io.updateStrm.valid.peek().litToBoolean
+        while(!valid) {
+          c.clock.step()
+          valid = c.io.updateStrm.valid.peek().litToBoolean
+        }
+
+        for(i <- 0 until nFeatures) {
+          c.io.updateStrm.valid.expect(true.B)
+          c.io.updateStrm.bits.w.expect(W(i).S(p.WeightWidth))
+          c.io.updateStrm.bits.x.expect(X(i).S(p.XWidth))
+          c.clock.step()
+        }
+      }
+    }.join()
+
+  }
+}
+
+implicit class UpdatePEDUT(c : UpdatePE)(implicit p : MixerParameter) {
+
+  def init() = {
+    c.io.updateStrm.initSource()
+    c.io.updateStrm.setSourceClock(c.clock)
+
+    c.io.wStrm.initSink()
+    c.io.wStrm.setSinkClock(c.clock)
+  }
+
+  def test(dbFile : String) = {
+    val reader = CSVReader.open(new File(dbFile))
+    val data = reader.all().map{ _.map(_.toInt)}.toIndexedSeq
+    val n = p.nFeatures
+    // line
+    // X(n), W(n), Wu(n), loss
+    fork {
+      // feed X, W, loss
+      val xfrom = 0
+      val xuntil = xfrom + n
+      val wfrom = n
+      val wuntil = wfrom + n
+      val lossIdx = n * 3
+      for(line <- data) {
+        val X = line.slice(xfrom, xuntil)
+        val W = line.slice(wfrom, wuntil)
+        val loss = line(lossIdx)
+
+        c.io.loss.poke(loss.S(p.lossWidth))
+        for(i <- 0 until n) {
+          c.io.updateStrm.valid.poke(true.B)
+          c.io.updateStrm.bits.w.poke(W(i).S(p.WeightWidth))
+          c.io.updateStrm.bits.x.poke(X(i).S(p.XWidth))
+          c.clock.step()
+        }
+        c.io.updateStrm.valid.poke(false.B)
+      }
+    }.fork {
+      // expect Wu
+      val sfrom = n * 2
+      val suntil = sfrom + n
+      for(line <- data) {
+        val data = line.slice(sfrom, suntil)
+        for(w <- data) {
+          c.io.wStrm.expectDequeue(w.S(p.WeightWidth))
+        }
+      }
+    }.join()
+  }
+}
+
+private object MixerDUT {
+  def init(c : Mixer) = {
+    for(i <- 0 until 8) {
+      c.io.in(i).initSource()
+      c.io.in(i).setSourceClock(c.clock)
+      c.io.out(i).initSink()
+      c.io.out(i).setSinkClock(c.clock)
+      c.io.in(i).valid.poke(false.B)
+    }
+
+    c.reset.poke(true.B)
+    c.clock.step(16)
+    c.reset.poke(false.B)
+    c.clock.step()
+  }
+
+  def test(c : Mixer, db_file : String) = {
+    val reader = CSVReader.open(new File(db_file))
+
+    val data = reader.all().map{ _.map(_.toInt)}.toIndexedSeq
+    assert(data.length % 8 == 0)
+
+    var thr = fork {}
+    for(i <- 0 until 8) {
+      thr = thr.fork {
+        val exp = new BitProbBundle()
+        for(idx <- 0 until (data.length / 8)) {
+          val t = data(idx * 8 + i).takeRight(2)
+          val bit = t(0)
+          val prob = t(1)
+
+          c.io.out(i).expectDequeue(exp.Lit(
+            _.bit -> bit.U,
+            _.prob -> prob.U,
+            _.last -> (idx == (data.length / 8 - 1)).B
+          ))
+        }
+      }
+
+      thr = thr.fork {
+        val in = new BitProbsCtxBundle(c.nProb)
+        for(idx <- 0 until (data.length / 8)) {
+          assert(data(idx * 8 + i)(0) == i)
+          val t = data(idx * 8 + i).drop(1)
+          val probs = t.take(c.nProb)
+          val ctx = t(c.nProb)
+          val bit = t(c.nProb + 1)
+
+          c.io.in(i).enqueue(in.Lit(
+            _.probs -> Vec.Lit(probs.map(_.U(12.W)):_*),
+            _.ctx -> ctx.U,
+            _.bit -> bit.U,
+            _.last -> (idx == (data.length / 8 - 1)).B
+          ))
+        }
+      }
+    }
+    
+    thr.join()
+  }
+}
+
+}
